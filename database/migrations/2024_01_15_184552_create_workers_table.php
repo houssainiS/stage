993b784450877ed267a60032fbfa89f1f73a491c
houssainiS <houssaini.hs@gmail.com>
2024-01-15 19:49:00 +0100

    created database and workers table

---
+++ database/migrations/2024_01_15_184552_create_workers_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('workers', function (Blueprint $table) {
+        $table->id();
+        $table->string('username')->unique();
+        $table->string('name');
+        $table->string('last_name');
+        $table->string('email')->unique();
+        $table->string('password');
+        $table->integer('age');
+        $table->integer('cin');
+        $table->string('phone_number');
+        $table->integer('rank');
+        $table->string('rank_code');
+        $table->string('department');
+        $table->integer('requests_number');
+        $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('workers');
+    }
+};
