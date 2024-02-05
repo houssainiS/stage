ef1cb270e4c20f53f6d5ac3822631aa51820da39
houssainiS <houssaini.hs@gmail.com>
2024-02-05 02:57:17 +0100

    made DAF duration historys

--- resources/views/DAFpage/STdafHistory.blade.php
+++ resources/views/DAFpage/STdafHistory.blade.php
@@ -16,6 +16,8 @@
       <th scope="col">type</th>
       <th scope="col">date</th>
       <th scope="col">dh approval</th>
+      <th scope="col">Finished or not</th>
+      <th scope="col">Duration</th>
     </tr>
   </thead>
   <tbody>
@@ -32,6 +34,43 @@
       @else
       <p>waiting</p>
       @endif</td>
+      <td>@if($item->AMLC_bought == "True")
+        <p style="color:lime">Done !</p>
+        @else
+        not yet!
+        @endif
+      </td>
+      <td>
+        @if(isset($item->AMLC_bought_date))
+        @php
+// Create DateTime instances from the date strings
+$AMLC_bought_date = new DateTime($item->AMLC_bought_date);
+$created_at = new DateTime($item->created_at);
+
+// Get the interval between the two dates
+$interval = $AMLC_bought_date->diff($created_at);
+
+// Calculate total hours (including days)
+$totalHours = $interval->days * 24 + $interval->h;
+
+// Loop through each day and subtract if it is a weekend
+for ($i = 0; $i < $interval->days; $i++) {
+    // Modify the date by adding one day
+    $AMLC_bought_date->modify('+1 day');
+    // Get the day of the week as a number (0 for Sunday, 6 for Saturday)
+    $dayOfWeek = $AMLC_bought_date->format('w');
+    // Subtract 24 hours if it is a weekend
+    if ($dayOfWeek == 0 || $dayOfWeek == 6) {
+        $totalHours -= 24;
+    }
+}
+
+// Display the duration in the view blade
+echo "$totalHours hours";
+@endphp
+
+@endif
+</td>
     </tr>
    @endforeach
   </tbody>
